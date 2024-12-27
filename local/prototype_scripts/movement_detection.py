f96053c5a87badb65dc81285fe4cb597cb57a66a
Dave Starmer <dave@starmer.org.uk>
2024-12-27 12:56:51 +0000

    style: implement Black code formatter
    - set vscode to use Black for this workspace
    - reformat code to Black specifications

--- local/prototype_scripts/movement_detection.py
+++ local/prototype_scripts/movement_detection.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import cv2
 
-filename = 'non_vcs_video/output_20241209231543.mp4'
+filename = "non_vcs_video/output_20241209231543.mp4"
 filename_path = Path(filename)
 out_filename = filename_path.with_stem(filename_path.stem + "_processed")
 det_filename = filename_path.with_stem(filename_path.stem + "_detected")
@@ -31,7 +31,7 @@ colour_frames = []
 blur_ksize = (15, 15)
 
 # background subtractor
-bg_sub = cv2.createBackgroundSubtractorMOG2(history=20,varThreshold=25)
+bg_sub = cv2.createBackgroundSubtractorMOG2(history=20, varThreshold=25)
 
 # video codec
 # fourcc = cv2.VideoWriter_fourcc(*"mp4v")
@@ -73,7 +73,9 @@ while True:
 
         # compare deltas
         overall_delta = cv2.absdiff(delta1, delta2)
-        threshold_frame = cv2.threshold(overall_delta, detection_threshold, 255, cv2.THRESH_BINARY)[1]
+        threshold_frame = cv2.threshold(
+            overall_delta, detection_threshold, 255, cv2.THRESH_BINARY
+        )[1]
 
         # fdiff_frame = bg_sub_frames[current_frame]
         # fdiff_frame = threshold_frame
@@ -83,18 +85,20 @@ while True:
 
         # sort contours
         if video.get(cv2.CAP_PROP_POS_FRAMES) > 2:
-            contours, _ = cv2.findContours(fdiff_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            contours, _ = cv2.findContours(
+                fdiff_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+            )
 
             for contour in contours:
                 x, y, w, h = cv2.boundingRect(contour)
                 if w * h > alert_contour_area:
-                    cv2.rectangle(out_frame, (x,y), (x+w, y+h), (0,255,0), 2)
+                    cv2.rectangle(out_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         # output frame to file
         out_vid.write(out_frame)
         cv2.imshow("Test", out_frame)
 
-        out_det_frame = cv2.cvtColor(bg_sub_frames[DIFF_FRAMES[1]],cv2.COLOR_GRAY2BGR)
+        out_det_frame = cv2.cvtColor(bg_sub_frames[DIFF_FRAMES[1]], cv2.COLOR_GRAY2BGR)
         det_vid.write(out_det_frame)
         cv2.imshow("Detection", out_det_frame)
 
@@ -103,7 +107,7 @@ while True:
         colour_frames = colour_frames[1:]
         bg_sub_frames = bg_sub_frames[1:]
 
-    if cv2.waitKey(1) == ord('q'):
+    if cv2.waitKey(1) == ord("q"):
         break
 
 video.release()
