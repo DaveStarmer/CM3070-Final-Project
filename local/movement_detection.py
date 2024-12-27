58d4f2444b9d3605c6c489fefa95134c80040bb4
Dave Starmer <dave@starmer.org.uk>
2024-12-27 12:56:51 +0000

    style: implement Black code formatter
    - set vscode to use Black for this workspace
    - reformat code to Black specifications

--- local/movement_detection.py
+++ local/movement_detection.py
@@ -1,15 +1,23 @@
-from pathlib import Path
+"""Movement Detection"""
+
 from datetime import datetime
 import cv2
 
-FRAME_HISTORY=20
-ADDITIONAL_PROCESSING_SECS=0
+FRAME_HISTORY = 20
+ADDITIONAL_PROCESSING_SECS = 0
+
 
 class MovementDetection:
-    """Detect movement in video made up of supplied frame
-    """
-    def __init__(self, frame_rate=30, detection_threshold=50, alert_contour_area=10000,
-                 video_preroll=3, video_length=20):
+    """Detect movement in video made up of supplied frame"""
+
+    def __init__(
+        self,
+        frame_rate=30,
+        detection_threshold=50,
+        alert_contour_area=10000,
+        video_preroll=3,
+        video_length=20,
+    ):
         # frame rate of video supplied
         self._frame_rate = frame_rate
         # contour area to consider when alerting
@@ -21,11 +29,14 @@ class MovementDetection:
         # full colour video
         self._video = []
         # detection instance
-        self._bg_sub = cv2.createBackgroundSubtractorMOG2(history=FRAME_HISTORY,varThreshold=25)
+        self._bg_sub = cv2.createBackgroundSubtractorMOG2(
+            history=FRAME_HISTORY, varThreshold=25
+        )
         # time movement detected
         self._movement_dt = None
         # count number of frames captured
         self._captured_frames = 0
+        self._detection_threshold = detection_threshold
 
     def add_frame(self, frame):
         """Add frame to to movement detection
@@ -39,6 +50,10 @@ class MovementDetection:
         self._video.append(frame)
         det_frame = self._bg_sub.apply(frame)
 
+        # ensure that background subtraction has initialised
+        if len(self._video) < self.preroll:
+            return False
+
         moving_objects = self._detect_movement(det_frame)
         if moving_objects:
             self._draw_rectangles(moving_objects)
@@ -47,14 +62,16 @@ class MovementDetection:
                 self._movement_dt = datetime.now()
         if self._movement_dt is not None:
             self._captured_frames += 1
-        if self._captured_frames >= self.video_length * self._frame_rate - self.preroll:
+        if self._captured_frames + self.preroll >= self.video_length * self._frame_rate:
             self._tidy()
             return True
         self._tidy()
         return False
 
     def _detect_movement(self, frame):
-        contours, _ = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv2.findContours(
+            frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
         movement_points = []
         for contour in contours:
             x, y, w, h = cv2.boundingRect(contour)
@@ -68,15 +85,17 @@ class MovementDetection:
     def _draw_rectangles(self, boundaries):
         """Draw provided rectangles onto image"""
         for point1, point2 in boundaries:
-            cv2.rectangle(self._video[-1], point1, point2, (0,255,0), 2)
+            cv2.rectangle(self._video[-1], point1, point2, (0, 255, 0), 2)
 
     def _tidy(self):
-        frames_to_retain = int(self._frame_rate * (self.video_length + ADDITIONAL_PROCESSING_SECS))
+        frames_to_retain = int(
+            self._frame_rate * (self.video_length + ADDITIONAL_PROCESSING_SECS)
+        )
         if len(self._video) > frames_to_retain:
             self._video = self._video[-frames_to_retain:]
 
     def get_video(self):
-        # if (datetime.now() - self._movement_dt).total_seconds() - self.preroll >= self.video_length:
+        """Get video clip"""
         self._movement_dt = None
         self._captured_frames = 0
         return self._video
