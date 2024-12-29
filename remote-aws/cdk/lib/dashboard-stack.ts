bcd2de35e64120f42eca4e2cca4a9dda083624f0
Dave Starmer <dave@starmer.org.uk>
2024-12-29 12:28:37 +0000

    update dashboard stack name

--- remote-aws/cdk/lib/dashboard-stack.ts
+++ remote-aws/cdk/lib/dashboard-stack.ts
@@ -5,6 +5,7 @@ import { Construct } from 'constructs';
 export class DashboardStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
     new Bucket(this, "video-bucket", {
       bucketName:"video-bucket"
     })
