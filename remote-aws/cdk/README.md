48a4254c4f19c83c0fcae613fc5874e43f4f7b10
Dave Starmer <dave@starmer.org.uk>
2024-12-29 11:09:01 +0000

    feat: initial CDK project for dashboard

---
+++ remote-aws/cdk/README.md
@@ -0,0 +1,2 @@
+# Dashboard CDK
+Dashboard CDK Project
---
+++ remote-aws/cdk/lib/dashboard-stack.ts
@@ -0,0 +1,13 @@
+import * as cdk from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+
+export class DashboardStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new Bucket(this, "mybucket", {
+      bucketName:"abucket"
+    })
+  }
+}
