da9377e3356652c07a23cdd3ac0cb33276fcd0c9
Dave Starmer <dave@starmer.org.uk>
2024-12-30 06:25:30 +0000

    trigger workflow

--- remote-aws/cdk/lib/dashboard-stack.ts
+++ remote-aws/cdk/lib/dashboard-stack.ts
@@ -5,7 +5,6 @@ import { Construct } from 'constructs';
 export class DashboardStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
     new Bucket(this, "video-bucket", {
       bucketName:"video-bucket"
     })
