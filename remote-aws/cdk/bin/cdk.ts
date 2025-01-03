bec05574bc51a6652a7764fdfd2cc255231c7709
Dave Starmer <dave@starmer.org.uk>
2025-01-03 21:23:41 +0000

    add basic instructions structure

--- remote-aws/cdk/bin/cdk.ts
+++ remote-aws/cdk/bin/cdk.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { DashboardStack } from '../lib/dashboard-stack';
+import { InstructionsStack } from '../lib/instructions-stack';
 
 const app = new cdk.App();
 new DashboardStack(app, 'DashboardStack', {
@@ -17,4 +18,6 @@ new DashboardStack(app, 'DashboardStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+})
+
+new InstructionsStack(app, 'InstructionsStack', {})
---
+++ remote-aws/cdk/lib/cloudfront-stack.ts
@@ -0,0 +1,15 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { HostedZone } from "aws-cdk-lib/aws-route53";
+import { Construct } from "constructs";
+import { Domain } from "domain";
+
+
+export class CloudFrontStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
+
+    const codeBucket = new Bucket ()
+
+    const domain = new Domain ()
+  }
+}
