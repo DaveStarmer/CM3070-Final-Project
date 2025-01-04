bc08d879c5fe046b8b7f10d81f6f76b89039a4d3
Dave Starmer <dave@starmer.org.uk>
2025-01-04 11:47:22 +0000

    make remove_kvp recursive

--- build/strip_cdk.py
+++ build/strip_cdk.py
@@ -4,18 +4,21 @@ from pathlib import Path
 import json
 
 
-def remove_kvp(dictionary: dict, key: str | int):
+def remove_kvp(dictionary: dict, keys: list):
     """Remove Key-Value Pair from Dictionary
 
     Args:
         dictionary (dict): source dictionary
-        key (str | int): key to remove if present
+        key (list): list of keys in path to pair to remove if present
 
     Returns:
         dict: modified dictionary
     """
-    if key in dictionary:
-        dictionary.pop(key)
+    if keys[0] in dictionary:
+        if len(keys) == 1:
+            dictionary.pop(keys)
+        else:
+            dictionary[keys[0]] = remove_kvp(dictionary[keys[0]], keys[1:])
     return dictionary
 
 
@@ -25,17 +28,15 @@ def remove_cdk_requirements(filename: str):
     Args:
         filename (str): filename of file to cleanse
     """
-    f = open(filename)
+    f = open(filename, encoding="utf-8")
     json_dict = json.load(f)
     f.close()
 
-    json_dict["Resources"] = remove_kvp(json_dict["Resources"], "CDKMetadata")
-    json_dict["Conditions"] = remove_kvp(
-        json_dict["Conditions"], "CDKMetadataAvailable"
-    )
-    json_dict["Parameters"] = remove_kvp(json_dict["Parameters"], "BootstrapVersion")
-    json_dict["Rules"] = remove_kvp(json_dict["Rules"], "CheckBootstrapVersion")
-    with open(filename, "w") as f:
+    json_dict = remove_kvp(json_dict, ["Resources", "CDKMetadata"])
+    json_dict = remove_kvp(json_dict, ["Conditions", "CDKMetadataAvailable"])
+    json_dict = remove_kvp(json_dict, ["Parameters", "BootstrapVersion"])
+    json_dict = remove_kvp(json_dict, ["Rules", "CheckBootstrapVersion"])
+    with open(filename, "w", encoding="utf-8") as f:
         json.dump(json_dict, f, indent=1)
     print(f"{filename} cleansed")
 
