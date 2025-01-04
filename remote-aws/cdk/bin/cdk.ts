f3a5229aea69c81dcb0d3a0b6a0657a457c72a23
Dave Starmer <dave@starmer.org.uk>
2025-01-04 12:16:54 +0000

    correct deployment name of deployment stack

--- remote-aws/cdk/bin/cdk.ts
+++ remote-aws/cdk/bin/cdk.ts
@@ -12,22 +12,24 @@ new DeploymentStack(app, "DeploymentStack")
 new CloudFrontStack(app, 'CloudFrontStack', {
   env: {
     region: 'us-east-1'
-}})
+  }
+})
 
-new DashboardStack(app, 'DashboardStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
+new DashboardStack(app, 'DashboardStack', {})
+
+new InstructionsStack(app, 'InstructionsStack')
+
+/* Notes for stack environments - delete before submission */
+/* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+/* Uncomment the next line to specialize this stack for the AWS Account
+ * and Region that are implied by the current CLI configuration. */
+// env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+/* Uncomment the next line if you know exactly what Account and Region you
+ * want to deploy the stack to. */
+// env: { account: '123456789012', region: 'us-east-1' },
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-})
-
-new InstructionsStack(app, 'InstructionsStack')
+/* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
