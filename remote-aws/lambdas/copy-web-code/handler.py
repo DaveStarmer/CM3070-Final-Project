ac3b29fb0f9321919d411ecc0fafc02be54037f7
Dave Starmer <dave@starmer.org.uk>
2025-02-09 13:57:25 +0000

    use crhelper to create custom resource

--- remote-aws/lambdas/copy-web-code/handler.py
+++ remote-aws/lambdas/copy-web-code/handler.py
@@ -3,57 +3,43 @@ Code below is copied with fewer comments into the cloudfront stack, as copying f
 buckets in different regions
 """
 
-# print statements throughout appear in CloudWatch Logs as log entries
-
 import os
 from pathlib import Path
-import json
-import requests
 import boto3
+from crhelper import CfnResource
+import logging
+
+# intialise logging
+logger = logging.getLogger(__name__)
+
+# Custom Resource helper
+helper = CfnResource()
 
 
-def handler_function(event: dict, *_):
-    """Entry Point for Lambda to copy code to web buckets"""
-    try:
-        if event["RequestType"].upper() == "DELETE":
-            print("DELETE REQUEST")
-            if send_response(event, True):
-                print("Delete OK")
-                return "Delete OK"
-            else:
-                print("Delete HTTP call failed")
-                return "Delete HTTP call failed."
-
-        # CREATE or UPDATE request
-        # copy files across as necessary, and return success (return fail if exception raised)
-        print("CREATE/UPDATE REQUEST")
-        copy_files(event["ResourceProperties"])
-    except Exception as e:
-        send_response(event, False)
-        print("Create/Update FAILED")
-        print(e)
-        return "Create/Update FAILED"
-
-    send_response(event, True)
-    print("Create/Update SUCCEDED")
-    return "Create/Update SUCCEDED"
-
-
-def copy_files(props: dict):
+@helper.create
+@helper.update
+def copy_files(event: dict, _):
     """Copy files between buckets
 
     Args:
-        props (Dict): properties
+        props (dict): properties
+        _: context, not used
     """
+
+
+def helper_create_update(event, _):
+    # properties used by lambda
+    props = event["ResourceProperties"]
+
     # region for source bucket
     source_region = props.get(
         "sourceRegion",
         os.environ.get("AWS_REGION", os.environ.get("AWS_DEFAULT_REGION")),
     )
-    print(f"Source Region: {source_region}")
+    logger.info(f"Source Region: {source_region}")
     # region for destination bucket
     dest_region = props.get("destinationRegion", source_region)
-    print(f"Destination Region: {dest_region}")
+    logger.info(f"Destination Region: %s", dest_region)
 
     # source code bucket
     source_bucket_name = props["sourceBucket"]
@@ -61,7 +47,7 @@ def copy_files(props: dict):
     s3_source = boto3.resource("s3", region=source_region)
     # create bucket resource for source bucket
     source_bucket = s3_source.Bucket(source_bucket_name)
-    print(f"Source Bucket: {source_bucket_name}")
+    logger.info(f"Source Bucket: %s", source_bucket_name)
 
     # destination code buckets
     dest_bucket_name = props["destinationBucket"]
@@ -69,7 +55,7 @@ def copy_files(props: dict):
     s3_dest = boto3.resource("s3", region=dest_region)
     # create bucket resources for public and private web buckets
     dest_bucket = s3_dest.Bucket(dest_bucket_name)
-    print(f"Destination Bucket: {dest_bucket_name}")
+    logger.info(f"Destination Bucket: %s", dest_bucket_name)
 
     # iterate contents of source bucket, and copy contents across
     # without parent folder name
@@ -84,23 +70,20 @@ def copy_files(props: dict):
                 if new_key == "":
                     new_key = Path(file).name
                 dest_bucket.copy(source, new_key)
-                print(f"copied {file.key} to {new_key}")
+                logger.info("copied %s to %s", file.key, new_key)
 
 
-def send_response(event: dict, success: bool) -> bool:
-    response_url = event["ResponseURL"]
+@helper.delete
+def no_op(_, __):
+    """Function to execute on delete - performs no function"""
+    pass
 
-    payload = {
-        "PhysicalResourceId": event["LogicalResourceId"],
-        "StackId": event["StackId"],
-        "RequestId": event["RequestId"],
-        "LogicalResourceId": event["LogicalResourceId"],
-    }
 
-    if success:
-        payload["Status"] = "SUCCESS"
-    else:
-        payload["Status"] = "FAILED"
+def handler_function(event, context):
+    """Lambda Function entry point
 
-    response = requests.put(response_url, json=payload)
-    print(response)
+    Args:
+        event (dict): AWS Lambda event
+        context (AWS context): AWS Lambda context
+    """
+    helper(event, context)
