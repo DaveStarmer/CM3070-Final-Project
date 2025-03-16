6d28e0b05af3bfeb2b6bb47424fd1f209748574c
Dave Starmer <dave@starmer.org.uk>
2025-03-16 14:37:07 +0000

    update lambda deployment

--- remote-aws/cdk/lib/cloudfront-stack.ts
+++ remote-aws/cdk/lib/cloudfront-stack.ts
@@ -6,14 +6,13 @@ import { CfnUserPoolUser, LambdaVersion, UserPool, UserPoolClient, UserPoolDomai
 import { CanonicalUserPrincipal, CompositePrincipal, Effect, ManagedPolicy, PolicyStatement, Role, ServicePrincipal, User } from "aws-cdk-lib/aws-iam"
 import { ApplicationLogLevel, Code, Function, IVersion, LoggingFormat, Runtime, Version } from "aws-cdk-lib/aws-lambda"
 import { ARecord, HostedZone, RecordTarget } from "aws-cdk-lib/aws-route53"
-import { Bucket, EventType, HttpMethods } from "aws-cdk-lib/aws-s3"
+import { Bucket, EventType, HttpMethods, IBucket } from "aws-cdk-lib/aws-s3"
 import { Construct } from "constructs"
 import { Domain } from "domain"
 import { UserPoolUser } from "./constructs/UserPoolUser"
 import { UserPoolDomainTarget } from "aws-cdk-lib/aws-route53-targets"
 import { Secret } from "aws-cdk-lib/aws-secretsmanager"
 import { ParameterDataType, ParameterTier, StringParameter } from "aws-cdk-lib/aws-ssm"
-// import { EdgeFunction } from "aws-cdk-lib/aws-cloudfront/lib/experimental"
 
 // orig version of cdk 2.173.4
 
@@ -21,11 +20,11 @@ export class CloudFrontStack extends Stack {
     publicWebBucket: Bucket
     privateWebBucket: Bucket
     certificate: ICertificate
-    codeBucket: any
+    codeBucket: IBucket
     authLambda: experimental.EdgeFunction
     authLambdaVersion: IVersion
+    lambdaSuffix: string
     userPool: UserPool
-    userPoolDomain: UserPoolDomain
     userPoolClient: UserPoolClient
     userPoolInfoSecret: Secret
 
@@ -65,10 +64,6 @@ export class CloudFrontStack extends Stack {
 
         /** code bucket construct */
         this.codeBucket = Bucket.fromBucketName(this, "codeBucket", Fn.ref("codeBucketName"))
-        // this.codeBucket = Bucket.fromBucketAttributes(this, "codeBucket", {
-        //     bucketName: Fn.ref("codeBucketName"),
-        //     region: "us-east-1"
-        // })
 
         /** Registered Domain Name */
         const domainName = new CfnParameter(this, "domainName", {
@@ -106,26 +101,10 @@ export class CloudFrontStack extends Stack {
         /** bucket for private web content - dashboard */
         this.privateWebBucket = new Bucket(this, "privateWebBucket", {
             bucketName: Fn.sub("vid-dash-private-web-${uniqueId}"),
-            // cors: [{
-            //     allowedOrigins: ["https*"],
-            //     allowedMethods: [HttpMethods.GET, HttpMethods.POST],
-            //     allowedHeaders: ["*"],
-            //     exposedHeaders: ["Etag", "x-amx-meta-custom-header"]
-            // }],
             removalPolicy: RemovalPolicy.DESTROY
         })
 
-        // /** bucket for public web content */
-        // this.publicWebBucket = new Bucket(this, "publicWebBucket", {
-        //     bucketName: Fn.sub("vid-dash-public-web-${uniqueId}"),
-        //     cors: [{
-        //         allowedOrigins: ["http*"],
-        //         allowedMethods: [HttpMethods.GET],
-        //         allowedHeaders: ["*"],
-        //         exposedHeaders: ["Etag", "x-amx-meta-custom-header"]
-        //     }],
-        //     removalPolicy: RemovalPolicy.DESTROY
-        // })
+        this.lambdaSuffix = this.node.tryGetContext("lambdaSuffix")
 
         // create User Pool
         this.createUserPool()
@@ -162,28 +141,13 @@ export class CloudFrontStack extends Stack {
 
         userPool.node.addDependency(this.certificate)
 
-        // const userPoolDomain = new UserPoolDomain(this, "UserPoolDomain", {
-        //     userPool,
-        //     customDomain: {
-        //         domainName: Fn.sub("auth.${domainName}"),
-        //         certificate: this.certificate
-        //     }
-        //     // cognitoDomain: {
-        //     //     domainPrefix: 'ds-fyp',
-        //     // },
-        // })
 
         const userPoolDomain = userPool.addDomain("userPoolDashDomain", {
             customDomain: {
                 domainName: Fn.sub("auth.${domainName}"),
                 certificate: this.certificate
             }
-            // cognitoDomain: {
-            //     domainPrefix: Fn.ref("publicUniqueId")
-            // }
         })
-
-        // userPoolDomain.node.addDependency(userPool)
 
         const userPoolClient = userPool.addClient('dashUserPoolClient', {
             generateSecret: true,
@@ -247,24 +211,7 @@ export class CloudFrontStack extends Stack {
         })
         upClientOutput.node.addDependency(userPoolClient)
 
-        // const currentEpoch = (new Date()).getTime()
-        // const userPoolClientSecretName = `user-pool-secret-${currentEpoch}`
-
-        // new StringParameter(this, "userPoolClientSecretName", {
-        //     description: "Name of User Pool Client Secret",
-        //     dataType: ParameterDataType.TEXT,
-        //     tier: ParameterTier.STANDARD,
-        //     parameterName: "user-pool-client-secret-name",
-        //     stringValue: userPoolClientSecretName
-        // })
-
-        // new Secret(this, "userPoolClientSecret", {
-        //     secretName: userPoolClientSecretName,
-        //     secretStringValue: userPoolClient.userPoolClientSecret
-        // })
-
         this.userPool = userPool
-        // this.userPoolDomain = userPoolDomain
         this.userPoolClient = userPoolClient
     }
 
@@ -320,23 +267,11 @@ export class CloudFrontStack extends Stack {
             timeout: Duration.minutes(15),
             runtime: Runtime.PYTHON_3_13,
             handler: "handler.handler_function",
-            code: Code.fromBucketV2(this.codeBucket, "lambdas/copy-web-code.zip")
+            code: Code.fromBucketV2(this.codeBucket, `lambdas/copy-web-code-${this.lambdaSuffix}.zip`)
         })
 
         const grantToCodeBucket = this.codeBucket.grantRead(copyLambda)
-        // const grantToPublicBucket = this.publicWebBucket.grantPut(copyLambda)
         const grantToPrivateBucket = this.privateWebBucket.grantPut(copyLambda)
-
-        // const copyPublicWebResources = new CustomResource(this, "copyPublicWebResources", {
-        //     serviceToken: copyLambda.functionArn,
-        //     properties: {
-        //         sourceBucket: this.codeBucket.bucketName,
-        //         destinationBucket: this.publicWebBucket.bucketName,
-        //         keys: ["public-web"]
-        //     }
-        // })
-        // copyPublicWebResources.node.addDependency(grantToCodeBucket)
-        // copyPublicWebResources.node.addDependency(grantToPublicBucket)
 
         const copyPrivateWebResources = new CustomResource(this, "copyPrivateWebResources", {
             serviceToken: copyLambda.functionArn,
@@ -360,13 +295,13 @@ export class CloudFrontStack extends Stack {
             ...props,
             functionName: "edge-auth",
             runtime: Runtime.PYTHON_3_13,
-            code: Code.fromBucketV2(this.codeBucket, "lambdas/edge-auth.zip"),
+            code: Code.fromBucketV2(this.codeBucket, `lambdas/edge-auth-${this.lambdaSuffix}.zip`),
             timeout: Duration.seconds(5),
             handler: "handler.handler_function",
             role: lambdaRole,
             applicationLogLevelV2: ApplicationLogLevel.DEBUG,
             loggingFormat: LoggingFormat.JSON,
-            description: `Authorisation Edge Lambda, date to ensure latest version ${currentDate}`
+            description: "Authorisation Edge Lambda"
         })
 
         this.authLambdaVersion = this.authLambda.currentVersion
