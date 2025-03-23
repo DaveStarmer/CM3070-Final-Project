ace1f56cfff0caee4cdf24c68ae50696204b8794
Dave Starmer <dave@starmer.org.uk>
2025-03-23 11:26:34 +0000

    debug activation list handler and clear api

--- remote-dashboard/lambdas/activations-list/handler.py
+++ remote-dashboard/lambdas/activations-list/handler.py
@@ -10,14 +10,18 @@ logger = logging.getLogger(__name__)
 def handler_function(event, _):
     logger.debug(event)
     logger.debug(os.environ)
-    table_name = os.environ("DYNAMODB_TABLE")
-    http_method = event["requestContext"]["http"]["method"]
+    table_name = os.environ["DYNAMODB_TABLE"]
+    http_method = event["requestContext"]["httpMethod"]
 
     if http_method == "GET":
+        logger.debug("GET method")
         search_params = event["queryStringParameters"]
+        if search_params is None:
+            search_params = {}
     else:
+        logger.debug("Other method")
         search_params = json.loads(event["body"])
-    logger.debug(search_params)
+    logger.debug("Search params: %s", search_params)
 
     ddb_resource = boto3.resource("dynamodb")
     table = ddb_resource.Table(table_name)
@@ -29,4 +33,15 @@ def handler_function(event, _):
 
     response = table.scan(**kw_args)
 
-    logger.debug(response)
+    api_response = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": True,
+        },
+        "body": json.dumps(response["Items"]),
+    }
+
+    logger.debug("Response: %s", api_response)
+
+    return api_response
